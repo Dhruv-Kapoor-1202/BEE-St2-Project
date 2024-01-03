ddabe1ce1a7eb4f31a82379a6331da4fe8668c82
Dhruv-Kapoor-1202 <dhruv1690.be21@chitkara.edu.in>
2024-01-04 04:07:18 +0530

    widgets complete ig

---
+++ frontent/src/scenes/widgets/MyPostWidget.jsx
@@ -0,0 +1,131 @@
+import {
+  EditOutlined,
+  DeleteOutlined,
+  AttachFileOutlined,
+  GifBoxOutlined,
+  ImageOutlined,
+  MicOutlined,
+  MoreHorizOutlined,
+} from "@mui/icons-material";
+
+import Dropzone from "react-dropzone";
+import UserImage from "../../components/UserImage.jsx";
+
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { setPosts } from "../../state/index.js";
+
+const MyPostWidget = ({ picturePath }) => {
+  const dispatch = useDispatch();
+
+  const [isImage, setIsImage] = useState(false);
+  const [image, setImage] = useState(null);
+  const [post, setPost] = useState("");
+
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+
+  const handlePost = async () => {
+    const formData = new FormData();
+    formData.append("userId", _id);
+    formData.append("description", post);
+
+    if (image) {
+      formData.append("picture", image);
+      formData.append("picturePath", image.name);
+    }
+
+    const response = await fetch(`http://localhost:3001/posts`, {
+      method: "POST",
+      headers: {
+        Authorization: `Bearer ${token}`
+      },
+      body: formData,
+    });
+    const posts = await response.json();
+    dispatch(setPosts({ posts }));
+    setImage(null);
+    setPost("");
+  };
+
+  return (
+    <div>
+      <div>
+        <UserImage image={picturePath} />
+        <input
+          type="text"
+          placeholder="What's on your mind..."
+          className="input input-bordered input-lg w-full max-w-xs"
+          onChange={(e) => setPost(e.target.value)}
+          value={post}
+        />
+      </div>
+      {isImage && (
+        <div>
+          <Dropzone
+            acceptedFiles=".jpg,.jpeg,.png"
+            multiple={false}
+            onDrop={(acceptedFiles) => setImage(acceptedFiles[0])}
+          >
+            {({ getRootProps, getInputProps }) => (
+              <div>
+                <div
+                  {...getRootProps()}
+                >
+                  <input {...getInputProps()} />
+                  {!image ? (
+                    <p>Add Image Here</p>
+                  ) : (
+                    <div>
+                      <p>{image.name}</p>
+                      <EditOutlined />
+                    </div>
+                  )}
+                </div>
+                {image && (
+                  <button
+                    type="submit"
+                    className="btn"
+                    onClick={() => setImage(null)}
+                  >
+                    <DeleteOutlined />
+                  </button>
+                )}
+              </div>
+            )}
+          </Dropzone>
+        </div>
+      )}
+
+      {/* Divider */}
+
+      <div>
+        <div
+          onClick={() => setIsImage(!isImage)}
+        >
+          <ImageOutlined />
+          <p>
+            Image
+          </p>
+        </div>
+
+        {/* Is Non Moblie Screens */}
+
+
+
+
+        <button
+          className="btn btn-wide"
+          onClick={handlePost}
+          disabled={!post}
+        >
+          POST
+        </button>
+
+
+      </div>
+    </div>
+  );
+};
+
+export default MyPostWidget;
--- frontent/src/scenes/widgets/UserWidget.jsx
+++ frontent/src/scenes/widgets/UserWidget.jsx
@@ -69,10 +69,63 @@ const UserWidget = ({ userId, picturePath }) => {
       {/* Divider */}
 
       {/* Second Row */}
+      <div>
+        <div>
+          <LocationOnOutlined fontSize="large" />
+          <p>{location}</p>
+        </div>
+        <div>
+          <WorkOutlineOutlined fontSize="large" />
+          <p>{occupation}</p>
+        </div>
+      </div>
 
+      {/* Divider */}
 
+      {/* Third Row */}
+      <div>
+        <div>
+          <p>Who's viewed your profile</p>
+          <p>{viewedProfile}</p>
+        </div>
+        <div>
+          <p>Impressions of your post</p>
+          <p>{impressions}</p>
+        </div>
+      </div>
 
+      {/* Divider */}
 
+      {/* Fourth Row */}
+      <div>
+        <p>
+          Social Profiles
+        </p>
+
+        <div>
+          <div>
+            <img src="../assets/twitter.png" alt="twitter" />
+            <div>
+              <p>Twitter</p>
+              <p>Social Network</p>
+            </div>
+          </div>
+          <EditOutlined />
+        </div>
+
+        <div>
+          <div>
+            <img src="../assets/linkedin.png" alt="linkedin" />
+            <div>
+              <p>LinkedIn</p>
+              <p>Network Platform</p>
+            </div>
+          </div>
+          <EditOutlined />
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default UserWidget;
